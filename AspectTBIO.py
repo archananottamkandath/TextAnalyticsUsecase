034a52abb6bf53ae676fca5765d59ee2f1605077
Archana Nottamkandath <archana.n@nl.ibm.com>
2018-02-15 19:11:12 +0100

    Iteration 2

--- AspectTBIO.py
+++ AspectTBIO.py
@@ -78,15 +78,14 @@ def ParseXML(tree):
     return sentence_array
   
   
-# lesser features testing the accuracy,precision,recall - Iteration 1
+
+# More features testing the accuracy,precision,recall - Iteration 2
 def add_sent_features(sent):
     return [add_word_features(sent, i) for i in range(len(sent))]
     
 def add_sent_labels(sent):
     return [label for token, postag, label in sent]
-
-def add_sent_tokens(sent):
-    return [token for token, postag, label in sent]    
+  
 
 def add_word_features(sent,i):
     word = sent[i][0]
@@ -95,7 +94,9 @@ def add_word_features(sent,i):
         sentence_length = "long"
     else:
         sentence_length = "short"
+    #print word,postag
     features = [
+        'word.length=%i' % len(word),
         'word.lower=' + word.lower(),
         'sentence_length=%s'% sentence_length,
         'word.istitle=%s' % word.istitle(),
@@ -104,13 +105,40 @@ def add_word_features(sent,i):
         'word.isdigit=%s' % word.isdigit(),
         'postag=' + postag,
         'word.pos_in_sent=%s' % (i + 1),
+        'postag[:2]=' + postag[:2]
         #'bias',
         #'word[-3:]=' + word[-3:],
         #'word[-2:]=' + word[-2:],
         #'postag[:2]=' + postag[:2],
     ]
+    if i > 0:
+        word1 = sent[i-1][0]
+        postag1 = sent[i-1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:postag=' + postag1,
+            '-1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('__Begin__')
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        postag1 = sent[i+1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:postag=' + postag1,
+            '+1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('__End__')
     #print features
     return features
+
 
 
 train_set = ParseXML(ET.parse('Data/Laptops_Train_v2.xml'))
